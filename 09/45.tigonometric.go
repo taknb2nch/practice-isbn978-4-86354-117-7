c823957104ccd79d7a6ba5b5dac47da8562a730d
taknb2nch <taknb2nch@gmail.com>
2013-11-14 00:26:13 +0900

    第9章 section48 数学処理まで終了。

---
+++ 09/45.tigonometric.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	
+	var degree float64 = 45
+
+	radian := degree * math.Pi / 180
+
+	fmt.Println("角度(ラジアン):", radian)
+
+	fmt.Println("正弦:", math.Sin(radian))
+
+	fmt.Println("余弦:", math.Cos(radian))
+
+	fmt.Println("正接:", math.Tan(radian))
+
+}
